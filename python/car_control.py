648dcc71b134f876fec1ed78d570f57dc954bb10
Ammar Hamid <ammarhamid84@gmail.com>
2016-06-08 17:22:40 +0200

    Add start/stop engine
    Change-Id: I3659f5a64e44fbaffeda5061a601d9c1ba5da2e5

--- python/car_control.py
+++ python/car_control.py
@@ -9,6 +9,14 @@ pin_input_2 = 13  # true ==> forward
 pin_input_3 = 11
 pin_input_4 = 7  # true  ==> forward
 
+pin_enable_a = 16
+pin_enable_b = 18
+
+
+# TODO:
+# why the wheel on one side runs first ==> this cause problem on restart (too much power)
+# findout a little shock cause raspberry pi to restart !!!
+
 
 # init gpio pins
 def init():
@@ -17,10 +25,24 @@ def init():
     gpio.setup(pin_input_2, gpio.OUT)
     gpio.setup(pin_input_3, gpio.OUT)
     gpio.setup(pin_input_4, gpio.OUT)
+    gpio.setup(pin_enable_a, gpio.OUT)
+    gpio.setup(pin_enable_b, gpio.OUT)
 
 
 def cleanup():
     gpio.cleanup()
+
+
+def start_engine():
+    init()
+    gpio.output(pin_enable_a, True)
+    gpio.output(pin_enable_b, True)
+
+
+def stop_engine():
+    init()
+    gpio.output(pin_enable_a, False)
+    gpio.output(pin_enable_b, False)
 
 
 def forward():
@@ -32,6 +54,8 @@ def forward():
     # right side
     gpio.output(pin_input_2, True)
     gpio.output(pin_input_1, False)
+    time.sleep(2)
+    cleanup()
 
 
 def backward():
@@ -65,3 +89,7 @@ def right():
     # right side
     gpio.output(pin_input_2, True)
     gpio.output(pin_input_1, True)
+
+
+start_engine()
+forward()
